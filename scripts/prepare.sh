53ebd84da67bc7b992d56eb163c3d1c1bd77b73d
Jason N <admin@cyderize.org>
2021-03-30 16:27:31 +1100

    Set isysroot in macOS CFLAGS

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -16,8 +16,7 @@ if [[ "$CBC_PLATFORM" == "osx" ]]; then
     brew install bash dos2unix pkg-config
     ln -s /usr/local/bin/bash /usr/local/bin/coin-bash
     config_opts+=" --enable-cbc-parallel --tests none"
-    xcrun --sdk macosx --show-sdk-path
-    echo "::set-output name=cflags::-arch x86_64 -arch arm64"
+    echo "::set-output name=cflags::-arch x86_64 -arch arm64 -isysroot $(xcrun --sdk macosx --show-sdk-path)"
 elif [[ "$CBC_PLATFORM" == "linux" ]]; then
     sudo apt-get update
     sudo apt-get -y install dos2unix
